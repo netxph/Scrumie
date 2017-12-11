5ea959b5d014b196d652839e81a8fedcf39a7851
Marc Abraham VITALIS <marc-abraham.vitalis@amadeus.com>
2017-12-11 18:26:49 +0800

    refactor auth

--- router.js
+++ router.js
@@ -1,38 +1,14 @@
 let express = require("express");
-let jwt = require("jsonwebtoken");
+let auth = require("./auth.js");
 let sessionCtrl = require("./server/controllers/session.server.controller.js");
 let meetingCtrl = require("./server/controllers/meeting.server.controller.js");
 let accountCtrl = require("./server/controllers/account.server.controller.js");
 
 let router = express.Router();
 
-//authentication middleware
-router.use(function(req, res, next) {
-    let token = req.headers.authorization;
-
-    if(!req.url.startsWith("/session") && 
-        !req.url.startsWith("/account/register"))
-    {
-        if(!token) {
-            return res.status(401).send();
-        } else {
-            jwt.verify(token, "secret", function(error, decoded) {
-                if(error) {
-                    return res.status(401).send();
-                } else {
-                    req.decoded = decoded;
-                    next();
-                }
-            });
-        }
-    } else {
-        next();
-    }
-});
-
-router.get("/meeting", meetingCtrl.getAll);
-
 router.post("/session", sessionCtrl.create);
 router.post("/account/register", accountCtrl.register);
+
+router.get("/meeting", auth.check, meetingCtrl.getAll);
 
 module.exports = router;
